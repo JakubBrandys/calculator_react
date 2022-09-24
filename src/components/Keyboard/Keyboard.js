251c491ce576c1fd69574c221f386b4f52da0a43
Adam Wuss <adamwusspl@gmail.com>
2022-09-24 17:18:40 +0200

    feat: add Keyboard component

---
+++ src/components/Keyboard/Keyboard.js
@@ -0,0 +1,19 @@
+import Button from "../Button/Button"
+
+const Keyboard = () => {
+  return (
+    <>
+      <Button value="1" />
+      <Button value="2" />
+      <Button value="3" />
+      <Button value="4" />
+      <Button value="5" />
+      <Button value="6" />
+      <Button value="7" />
+      <Button value="8" />
+      <Button value="9" />
+    </>
+  )
+}
+
+export default Keyboard;
