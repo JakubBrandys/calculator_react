f1586046f7d1ea58e1ed957262e33cbc9569a68a
Adam Wuss <adamwusspl@gmail.com>
2022-09-27 18:52:34 +0200

    refactor: update ButtonNumber.js

--- src/components/ButtonNumber/ButtonNumber.js
+++
@@ -1,9 +0,0 @@
-import styled from "./Button.module.css"
-
-const ButtonNumber = ({ number }) => {
-    return (
-        <button className={styled.buttonNumber}>{number}</button>
-    )
-}
-
-export default ButtonNumber;
---
+++ src/components/Buttons/ButtonNumber/ButtonNumber.js
@@ -0,0 +1,5 @@
+import styled from "./ButtonNumber.module.css"
+
+const ButtonNumber = ({ number }) => <button className={styled.buttonNumber}>{number}</button>;
+
+export default ButtonNumber;
