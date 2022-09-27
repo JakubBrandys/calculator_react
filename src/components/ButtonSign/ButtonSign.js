f15d0f62217b51d73a547c956eaa4657cc00e565
Jakub Brandys <kuba.brandys99@gmail.com>
2022-09-27 18:06:09 +0200

    feat: create ButtonSign

---
+++ src/components/ButtonSign/ButtonSign.js
@@ -0,0 +1,9 @@
+import styled from "./ButtonSign.module.css"
+
+const ButtonSign = ({ sign }) => {
+  return (
+   <button className={styled.sign}>{sign}</button>
+  );
+};
+
+export default ButtonSign
