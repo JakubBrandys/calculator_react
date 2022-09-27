3a51242eb877815e08b8b92ae23ef54dfdf0abd5
Adam Wuss <adamwusspl@gmail.com>
2022-09-27 18:52:49 +0200

    refactor: update ButtonEqual.js

--- src/components/ButtonEqual/ButtonEqual.js
+++
@@ -1,9 +0,0 @@
-import styled from "./ButtonEqual.module.css";
-
-const ButtonEqual = ({ equal }) => {
-  return (
-    <button className={styled.equal}>{equal}</button>
-  );
-};
-
-export default ButtonEqual;
---
+++ src/components/Buttons/ButtonEqual/ButtonEqual.js
@@ -0,0 +1,4 @@
+import styled from "./ButtonEqual.module.css";
+
+const ButtonEqual = () => <button className={styled.equal}>=</button>
+export default ButtonEqual;
