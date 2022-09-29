c20e99915ac432528a4044a73615370411009dc9
Jakub Brandys <kuba.brandys99@gmail.com>
2022-09-29 11:21:11 +0200

    feat: create calculator style and add to calculator

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,12 +1,13 @@
 import Keyboard from "../Keyboard/Keyboard";
 import Screen from "../Screen/Screen";
+import styled from "./Calculator.module.css";
 
 const Calculator = () => {
   return (
-    <>
+    <div className={styled.calculator}>
       <Screen/>
       <Keyboard/>
-    </>
+    </div>
   );
 };
 
